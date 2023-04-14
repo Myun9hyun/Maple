f5ddac9162a1288ea16722a0d095f6939582c6f6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-14 15:15:50 +0900

    안녕

--- Cozem/page.py
+++ Cozem/page.py
@@ -1578,47 +1578,48 @@ else:
             main()
 
 elif choice == "피드백 남기기":
-    st.header("⏸유예기간 관리⏸")
-        FILE_PATH2 = 'data2.csv'
-        st.error('⚠️길드 간부진만 접근할 수 있는 메뉴입니다!⚠️')
-        password_input = st.number_input('비밀번호를 입력해주세요 : ',min_value=0, key='password5')
-        if password_input == password:
-            st.success('접근을 허용합니다')
-            options = ["유예자 추가➕", "유예자 조회🔎", "유예자 삭제✂", "데이터 초기화💣" ]
-            option = st.selectbox("기능 선택", options, key='select3')
-        # 파일에서 데이터 불러오기
-            def load_data2():
-                try:
-                    data2 = pd.read_csv(FILE_PATH2)
-                except FileNotFoundError:
-                    data2 = pd.DataFrame(columns=['Name', 'Why', 'Due to'])
-                return data2
+    st.write()
+    # st.header("⏸유예기간 관리⏸")
+    #     FILE_PATH2 = 'data2.csv'
+    #     st.error('⚠️길드 간부진만 접근할 수 있는 메뉴입니다!⚠️')
+    #     password_input = st.number_input('비밀번호를 입력해주세요 : ',min_value=0, key='password5')
+    #     if password_input == password:
+    #         st.success('접근을 허용합니다')
+    #         options = ["유예자 추가➕", "유예자 조회🔎", "유예자 삭제✂", "데이터 초기화💣" ]
+    #         option = st.selectbox("기능 선택", options, key='select3')
+    #     # 파일에서 데이터 불러오기
+    #         def load_data2():
+    #             try:
+    #                 data2 = pd.read_csv(FILE_PATH2)
+    #             except FileNotFoundError:
+    #                 data2 = pd.DataFrame(columns=['Name', 'Why', 'Due to'])
+    #             return data2
 
-            # 데이터를 파일에 저장하기
-            def save_data2(data2):
-                data2.to_csv(FILE_PATH2, index=False)
+    #         # 데이터를 파일에 저장하기
+    #         def save_data2(data2):
+    #             data2.to_csv(FILE_PATH2, index=False)
 
-            # 데이터 초기화 함수
-            def clear_data2():
-                global data2
-                data2 = pd.DataFrame(columns=['Name', 'Why', 'Due to'])
-                # 파일 삭제
-                os.remove(FILE_PATH2)
-            # 데이터 삭제 함수
-            def delete_data2(row_index):
-                global data2
-                data2 = data2.drop(index=row_index).reset_index(drop=True)
+    #         # 데이터 초기화 함수
+    #         def clear_data2():
+    #             global data2
+    #             data2 = pd.DataFrame(columns=['Name', 'Why', 'Due to'])
+    #             # 파일 삭제
+    #             os.remove(FILE_PATH2)
+    #         # 데이터 삭제 함수
+    #         def delete_data2(row_index):
+    #             global data2
+    #             data2 = data2.drop(index=row_index).reset_index(drop=True)
 
-            # 불러온 데이터를 전역 변수로 저장
-            data2 = load_data2()
-            def add_data2(name, why, period):
-                global data2
-                if name in data2['Name'].values:
-                    st.warning(f'{name} (은)는 이미 있는 이름이야!')
-                    return
-                data2 = data2.append({
-                    'Name': name, 
-                    'Why' : why,
-                    'Due to' : period
+    #         # 불러온 데이터를 전역 변수로 저장
+    #         data2 = load_data2()
+    #         def add_data2(name, why, period):
+    #             global data2
+    #             if name in data2['Name'].values:
+    #                 st.warning(f'{name} (은)는 이미 있는 이름이야!')
+    #                 return
+    #             data2 = data2.append({
+    #                 'Name': name, 
+    #                 'Why' : why,
+    #                 'Due to' : period
 
-                }, ignore_index=True)
+    #             }, ignore_index=True)
