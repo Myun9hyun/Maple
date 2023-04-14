08e0a1f6e519555b97455dc023bfc16dd62bca05
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-14 13:40:19 +0900

    키값 추가

--- Cozem/page.py
+++ Cozem/page.py
@@ -925,7 +925,7 @@ elif choice == "직위관리":
                     # if st.button('데이터 삭제'):
                         # 사용자로부터 삭제할 행 번호 입력받기
                         st.write(data3[['Info','Cozem', 'Day']])
-                        row_index = st.number_input('삭제하고 싶은 데이터의 번호를 입력해주세요', min_value=0, max_value=data3.shape[0]-1)
+                        row_index = st.number_input('삭제하고 싶은 데이터의 번호를 입력해주세요', min_value=0, max_value=data3.shape[0]-1, key='row1')
                         st.write("Enter를 입력하면 삭제됩니다.")
                         if st.button('데이터 삭제'):
                             # 해당 행이 존재할 경우, 행을 삭제
