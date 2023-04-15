9b3eafd87ed3db22ff8026fa99155b961d6085b6
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-15 14:19:19 +0900

    문구 수정

--- Cozem/page.py
+++ Cozem/page.py
@@ -407,7 +407,7 @@ elif choice == "직위관리":
                 option = st.selectbox("기능 선택", options)
                 
                 if option == "데이터 추가➕":
-                    select_name = st.selectbox('이름을 골라줘(❁´◡`❁)', options=data5['Name'].tolist(),key='main13')
+                    select_name = st.selectbox('추가할 이름을 골라줘(❁´◡`❁)', options=data5['Name'].tolist(),key='main13')
                     is_main_character = st.radio('본캐/부캐', ('본캐', '부캐'))
                     weekly_mission = st.number_input('주간 미션 점수를 입력해주세요', min_value=0)
                     suro = st.number_input('수로 점수를 입력해주세요', min_value=0)
