e064f4f90e6186f3a5d7ce3c737921ac415ac4b2
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-05-01 16:48:21 +0900

    수정

--- Cozem/page.py
+++ Cozem/page.py
@@ -462,14 +462,64 @@ elif choice == "직위관리":
 
 
         # def role(Role):
+        # def add_character_data(name, character_type, weekly_mission, suro, flag):
+        #     global data, data5
+        #     add_name(name)  # 입력된 이름을 데이터에 추가
+        #     if character_type == '본캐':
+        #         add_data(name,character_type, weekly_mission, suro, flag)
+        #     elif character_type == '부캐':
+        #         # main_name = st.text_input('본캐 이름을 입력하세요')
+        #         main_name = st.selectbox('본캐 이름을 골라줘', options=data5['Name'].tolist(), key='main12')
+        #         main_data = data.loc[data['Name'] == main_name]
+        #         if len(main_data) == 0:
+        #             st.warning(f"{main_name} (은)는 등록되어있지 않아!")
+        #             # return
+        #         else:
+        #             main_data_index = main_data.index[0]
+        #             suro_cozem = Suro_cozem(suro)
+        #             flag_cozem = Flag_cozem(flag)
+        #             cozem_total = suro_cozem + flag_cozem
+        #             data.loc[main_data_index, 'Cozem_Total'] += cozem_total
+        #             if main_data['Suro'].values[0] >= 4000:
+        #                 novel_value = main_data['Novel'].values[0]
+        #             else:
+        #                 novel_value = novel_p(weekly_mission, suro, flag)  # Novel 값 계산
+        #             if weekly_mission >= 2:
+        #                 novel_value = main_data['Novel'].values[0]
+        #             else:
+        #                 novel_value = novel_p(weekly_mission, suro, flag)
+        #             role = character_type
+        #             warning_count = 0
+        #             warning_main = data[(data['Novel'] == 'X') & (data['Role'] == '본캐')]
+        #             if name in warning_main['Name'].values:
+        #                 warning_count = warning_count + 1
+        #             # data = data.append({
+        #             data = data.concat({
+        #                 'Name': name, 
+        #                 'Weekly_Mission': weekly_mission, 
+        #                 'Suro': suro, 
+        #                 'Suro_Cozem': suro_cozem,
+        #                 'Flag': flag, 
+        #                 'Flag_Cozem': flag_cozem,
+        #                 'Cozem_Total': cozem_total,
+        #                 'Novel': novel_value,
+        #                 'Role' : role,
+        #                 'Main_Name' : main_name
+        #             }, ignore_index=True)
+        #     else:
+        #         st.warning(f"{character_type} (은)는 본캐/부캐가 아닙니다!")
+        # import pandas as pd
+
+        # data = pd.DataFrame(columns=['Name', 'Weekly_Mission', 'Suro', 'Suro_Cozem', 'Flag', 'Flag_Cozem', 'Cozem_Total', 'Novel', 'Role', 'Main_Name'])
+
         def add_character_data(name, character_type, weekly_mission, suro, flag):
-            global data, data5
+            global data
             add_name(name)  # 입력된 이름을 데이터에 추가
             if character_type == '본캐':
-                add_data(name,character_type, weekly_mission, suro, flag)
+                add_data(name, character_type, weekly_mission, suro, flag)
             elif character_type == '부캐':
                 # main_name = st.text_input('본캐 이름을 입력하세요')
-                main_name = st.selectbox('본캐 이름을 골라줘', options=data5['Name'].tolist(), key='main12')
+                main_name = st.selectbox('본캐 이름을 골라줘', options=data['Name'].tolist(), key='main12')
                 main_data = data.loc[data['Name'] == main_name]
                 if len(main_data) == 0:
                     st.warning(f"{main_name} (은)는 등록되어있지 않아!")
@@ -493,21 +543,22 @@ elif choice == "직위관리":
                     warning_main = data[(data['Novel'] == 'X') & (data['Role'] == '본캐')]
                     if name in warning_main['Name'].values:
                         warning_count = warning_count + 1
-                    # data = data.append({
-                    data = data.concat({
-                        'Name': name, 
-                        'Weekly_Mission': weekly_mission, 
-                        'Suro': suro, 
-                        'Suro_Cozem': suro_cozem,
-                        'Flag': flag, 
-                        'Flag_Cozem': flag_cozem,
-                        'Cozem_Total': cozem_total,
-                        'Novel': novel_value,
-                        'Role' : role,
-                        'Main_Name' : main_name
-                    }, ignore_index=True)
+                    new_row = pd.DataFrame({
+                        'Name': [name], 
+                        'Weekly_Mission': [weekly_mission], 
+                        'Suro': [suro], 
+                        'Suro_Cozem': [suro_cozem],
+                        'Flag': [flag], 
+                        'Flag_Cozem': [flag_cozem],
+                        'Cozem_Total': [cozem_total],
+                        'Novel': [novel_value],
+                        'Role': [role],
+                        'Main_Name': [main_name]
+                    })
+                    data = pd.concat([data, new_row], ignore_index=True)
             else:
                 st.warning(f"{character_type} (은)는 본캐/부캐가 아닙니다!")
+
 
         def download_xlsx(df, file_name):
             # 파일 확장자가 .xlsx가 아니면 파일명 끝에 .xlsx를 붙여줌
