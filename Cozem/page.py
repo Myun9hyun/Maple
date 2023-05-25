11273c48c44ebb92ff779178bfa4c68fbabcf26e
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-05-25 14:36:42 +0900

    사이드바 메뉴 백그라운드 색 변경

--- Cozem/page.py
+++ Cozem/page.py
@@ -100,7 +100,7 @@ with st.sidebar:
         "container": {"padding": "4!important", "background-color": "#fafafa"},
         "icon": {"color": "black", "font-size": "25px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#02ab21"},
+        "nav-link-selected": {"background-color": "#73B4EC"},
     }
     )
 
